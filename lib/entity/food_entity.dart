eee3e32560bcddcffca9c8a5fa322d4c36b7ce76
Farhan Permana <46885640+farhanpermana@users.noreply.github.com>
2021-02-09 07:54:56 +0700

    Create food_entity.dart

---
+++ lib/entity/food_entity.dart
@@ -0,0 +1,14 @@
+class FoodEntity {
+  String imageAsset;
+  String nama;
+  String harga;
+  String rating;
+  String deskripsi;
+
+  FoodEntity(
+      {this.imageAsset,
+        this.nama,
+        this.harga,
+        this.rating,
+        this.deskripsi});
+}
